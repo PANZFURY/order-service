46b98e1136cb5b93a566b572d1dc607b837fb16f
Mikhail <mikhailveqluw@gmail.com>
2025-12-06 22:14:16 +0300

    add OrderService implementation

--- src/main/java/practical/task/orderservice/OrderserviceApplication.java
+++ src/main/java/practical/task/orderservice/OrderserviceApplication.java
@@ -3,11 +3,15 @@ package practical.task.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "practical.task.orderservice.service.client")
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableJpaAuditing
 @EnableFeignClients(basePackages = "practical.task.orderservice.service.client")
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
