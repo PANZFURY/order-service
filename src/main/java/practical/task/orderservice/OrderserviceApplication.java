2dfdb177f79a11fcebfc26e81b6ee953795fd5e0
Mikhail <mikhailveqluw@gmail.com>
2025-12-07 19:32:58 +0300

    add jpa auditing annotation

--- src/main/java/practical/task/orderservice/OrderserviceApplication.java
+++ src/main/java/practical/task/orderservice/OrderserviceApplication.java
@@ -6,15 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "practical.task.orderservice.service.client")
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 @EnableJpaAuditing
-@EnableFeignClients(basePackages = "practical.task.orderservice.service.client")
-@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class OrderserviceApplication {
 
 	public static void main(String[] args) {
