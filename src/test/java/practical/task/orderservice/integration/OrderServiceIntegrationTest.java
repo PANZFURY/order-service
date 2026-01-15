9da5c16c323109c552a60d1720df85f64ee0442d
Mikhail <mikhailveqluw@gmail.com>
2026-01-15 15:17:58 +0300

    add integration test

---
+++ src/test/java/practical/task/orderservice/integration/OrderServiceIntegrationTest.java
@@ -0,0 +1,111 @@
+package practical.task.orderservice.integration;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import practical.task.orderservice.model.Item;
+import practical.task.orderservice.repository.ItemRepository;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class OrderServiceIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("orders")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    static WireMockServer wireMockServer;
+
+    @DynamicPropertySource
+    static void registerProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+
+        registry.add("user.service.url", () -> wireMockServer.baseUrl());
+    }
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    static void startWireMock() {
+        wireMockServer = new WireMockServer(8089);
+        wireMockServer.start();
+    }
+
+    @AfterAll
+    static void stopWireMock() {
+        wireMockServer.stop();
+    }
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @BeforeEach
+    void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+
+        Item item = new Item();
+        item.setName("iPhone 12");
+        item.setPrice(699);
+
+        itemRepository.save(item);
+
+        wireMockServer.stubFor(
+                get(urlPathMatching("/api/user/get/1"))
+                        .willReturn(aResponse()
+                                .withHeader("Content-Type", "application/json")
+                                .withStatus(200)
+                                .withBody("""
+                                        {
+                                          "id": 1,
+                                          "email": "test@example.com",
+                                          "firstName": "Dave",
+                                          "lastName": "Horrish"
+                                        }
+                                        """))
+        );
+    }
+
+
+    @Test
+    void testOrderCreation() throws Exception {
+        String json = """
+                {
+                  "userId": 1,
+                  "items": [
+                    { "itemId": 1, "quantity": 2 }
+                  ]
+                }
+                """;
+
+        mockMvc.perform(post("/api/orders/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.user.email").value("test@example.com"))
+                .andExpect(jsonPath("$.items[0].quantity").value(2));
+    }
+}
